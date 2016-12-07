8737708a2f284b4216b9a1fc71965e2f4698b9cd
Ben Mansour Med Rafik <benm.stm@gmail.com>
2016-12-07 14:02:09 +0100

    Update streamevent_health_checker.sh

--- streamevent_health_checker.sh
+++ streamevent_health_checker.sh
@@ -4,10 +4,14 @@
 PATH="/home/gerritadm/gerrit_site/streamevent_health_checker/"
 #stream event output file (will be erased everytime the cron hits)
 FILE="$PATHstream_event.tmp"
-#log file
+#logging related params
 LOG="$PATHstream_event_checker.log"
-#used to mark loging date
 NOW=$(date)
+
+#Gerrit related params
+SERVER="gerrit.server.com"
+USER="user"
+PORT="29418"
 
 #check wether the file exists and empty (if yes empty it)
 if [[ -s $FILE ]] ; then
@@ -17,5 +21,5 @@ else
         #restart gerrit (which gonna refresh the stream event) and run stream event listener into a local file to ensure that it is not hangig
         echo "[$NOW]: Gerrit hang suspected" >> $LOG
         cd /home/gerritadm/gerrit_site/ && ./bin/gerrit.sh restart >> $LOG
-        ssh -p 29418 user@gerrit.server.com gerrit stream-events >> $FILE &
+        ssh -p $PORT $USER@$SERVER gerrit stream-events >> $FILE &
 fi ;
